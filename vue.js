617a69c5351097142bf65b9b70f349a8a64a0f74
diego londoño <diegoferlondo13@gmail.com>
2023-05-04 19:47:54 -0500

    uso evento v-on:mousemove el cual al pasar el puntero del mouse por un area demarcada por un div, se nos mostrara las coordenadas de ubicación del puntero fue necesario el uso de metodos y la interpolación

--- vue.js
+++ vue.js
@@ -1,14 +1,27 @@
 new Vue({
     el: '#eventos',
     data: {
-        contador:0
+        x: 0,
+        y: 0
+       
+        //contador:0
     },
     methods: {
-        sumarUno: function (){
+        mostrarUbicacion: function (evento){
+            this.x= evento.clientX;
+            this.y= evento.clientY;
+        }
+       
+        /* alerta: function(mensaje){
+            alert(mensaje);
+        } */
+        
+               
+        /*sumarUno: function (){
             this.contador += 1;
         },
         restarUno: function (){
             this.contador -= 1;
-        }
+        } */
     }
 })
